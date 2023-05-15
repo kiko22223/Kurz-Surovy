4e217be2a1e80505b85749f51a2475a62662c657
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2023-05-15 23:20:22 +0200

    Added func for counting metrics and rounding ending numbers and deleting zeros and dots.

--- Kurz-Surovy/ConvertVzdialenostView.swift
+++ Kurz-Surovy/ConvertVzdialenostView.swift
@@ -51,23 +51,64 @@ struct ConvertVzdialenostView: View {
                 }
                 .pickerStyle(.segmented)
                 
-                Text("\(vstupnaVzdialenostMM)")
+                
             }
             
             Section("Metricke") {
-                Text("Km")
-                Text("m")
-                Text("cm")
-                Text("mm")
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"Km"))
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"cm"))
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"m"))
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"mm"))
             }
             Section("Metricke") {
-                Text("mi")
-                Text("Yd")
-                Text("Ft")
-                Text("In")
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"mi"))
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"Yd"))
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"Ft"))
+                Text(vypocitajVzdialenost(vzdialenost: vstupnaVzdialenostMM, output:"In"))
             }
         }
         .navigationTitle("Vzdialenost")
+    }
+    
+    func vypocitajVzdialenost(vzdialenost: Double, output: String) -> String {
+        var vystup : Double
+        switch output {
+        case "Km":
+            vystup = vzdialenost / 1000000
+            
+        case "m":
+            vystup = vzdialenost / 1000
+            
+        case "cm":
+            vystup = vzdialenost / 10
+            
+        case "mi":
+            vystup = vzdialenost / 1609344
+        
+        case "Yd":
+            vystup = vzdialenost / 914.4
+            
+        case "Ft":
+            vystup = vzdialenost / 304.8
+        
+        case "In":
+            vystup = vzdialenost / 25.4
+            
+        default:
+            vystup = vzdialenost
+        }
+        
+        var textovyVystup = String(format: "%.6f", vystup)
+        
+        while textovyVystup.last == "0" {
+            textovyVystup.removeLast()
+        }
+        
+        if textovyVystup.last == "." {
+            textovyVystup.removeLast()
+        }
+        
+        return "\(textovyVystup) \(output)"
     }
 }
 
