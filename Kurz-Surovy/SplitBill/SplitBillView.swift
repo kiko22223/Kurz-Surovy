1bde5620bbb25de5173b3d37592f034602ac1f6b
Radovan Bojkovsky <radovan.bojkovsky@pixwell.sk>
2023-06-28 21:36:21 +0200

    no message

---
+++ Kurz-Surovy/SplitBill/SplitBillView.swift
@@ -0,0 +1,39 @@
+
+//  SplitBill.swift
+//  Kurz-Surovy
+//
+//  Created by Christian Marušák on 27/06/2023.
+
+import SwiftUI
+
+struct SplitBillView: View {
+    var body: some View {
+        @State var amountOfPeople = 1.0
+        @State var amountToPay = 0.0
+
+        Form(content: {
+            SwiftUI.Section("Enter amount to pay"){
+                Text("Enter amount of bill")
+                TextField("How much of you together payed", value: $amountToPay, format: .number)
+                    .keyboardType(.decimalPad)
+            }
+            SwiftUI.Section("Split your bill"){
+                Text("Choose amount of people")
+                TextField("How much of you were together", value: $amountOfPeople, format: .number)
+                    .keyboardType(.decimalPad)
+
+            }
+            SwiftUI.Section("Final cost"){
+                Text("\(amountToPay / amountOfPeople)")
+                    .font(.largeTitle)
+            }
+        })
+
+    }
+}
+
+struct SplitBill_Previews: PreviewProvider {
+    static var previews: some View {
+        SplitBillView()
+    }
+}
