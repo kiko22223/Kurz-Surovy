cc07c6f5dd259d6046dad84c5d9e1e304da33888
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2023-07-04 10:50:51 +0200

    Added playgrounds file

---
+++ Kurz-Surovy/Playground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let days : [(name:String, order: Int)] = [("utorok", 1),("streda",2),("pondelok", 0),("piatok",4),("nedela",6),("sobota",5),("stvrtok", 3)]
+
+let sortedDays = days.sorted(by: { $0.order < $1.order })
+
+for day in sortedDays {
+    print("\(day.name.localizedCapitalized) je \(day.order + 1) den v tyzdni")
+}
+
+let dayNames = sortedDays.map{$0.name}
+
+print(dayNames)
