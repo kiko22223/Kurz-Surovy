ef98a8dcfcde8f0e496b8d45b332e235f2407b12
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2023-08-01 18:45:01 +0200

    commit

--- Kurz-Surovy/Encode/Encode.swift
+++ Kurz-Surovy/Encode/Encode.swift
@@ -11,12 +11,13 @@ import UniformTypeIdentifiers
 struct Encode: View {
     
     
-  
+    
     
     @State var hiddenMessage = ""
     let clipboard = UIPasteboard.general
-    @State var buttonTitle = "Copy to clipborad"
-
+    @State var copied = false
+    @State var codeLevel = 1
+    
     func alphabetInDictionary() -> [Character: Int] {
         let alphabet = "abcdefghijklmnopqrstuvwxyz1234567890?!"
         var alphabetDict = [Character: Int]()
@@ -36,26 +37,33 @@ struct Encode: View {
         for letter in word {
             arrayOfIndexes.append(dictionary[letter] ?? 0)
         }
-    return arrayOfIndexes
+        return arrayOfIndexes
     }
     
     
-        var body: some View {
+    var body: some View {
         Form(content: {
             SwiftUI.Section("Enter your message you want to hide") {
-                TextField("Enter message", text: $hiddenMessage)
-            }.disableAutocorrection(true)
-                .keyboardType(.alphabet)
-            SwiftUI.Section("Your coded message in numbers"){
+                TextField("Enter message", text: $hiddenMessage).disableAutocorrection(true)
+                    .keyboardType(.alphabet)
+                
+                TextField("Select your code level", text: $codeLevel, format: .number).keyboardType(.decimalPad)
+            }
+                SwiftUI.Section("Your coded message in numbers"){
                 Text(String("\(codeString(word:hiddenMessage).map{String($0)}.joined(separator: " "))"))
-                Button(buttonTitle) {
-                    clipboard.string = (String("\(codeString(word:hiddenMessage).map{String($0)}.joined(separator: " "))"))
-                    buttonTitle = "Copied!"
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                     buttonTitle = "Copy To clipboard"
+                if copied {
+                    Text("Copied!").foregroundColor(.red).bold().font(.title)
+                } else {
+                    Button("Copy To clipboard") {
+                        clipboard.string = (String("\(codeString(word:hiddenMessage).map{String($0)}.joined(separator: " "))"))
+                        copied = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            copied = false
+                        }
                     }
                 }
             }
+            
         })
     }
 }
