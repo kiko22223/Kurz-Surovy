ea5a3f6a98eccf09b829d3ddf11700b6e3453ffb
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2023-08-15 10:16:52 +0200

    added compute method into decode view

--- Kurz-Surovy/Encode/DecodeView.swift
+++ Kurz-Surovy/Encode/DecodeView.swift
@@ -9,30 +9,51 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 struct DecodeView: View {
-    @State private var numbersMessage : String = ""
-    let decodeFuncs = Decode()
     @State var copied = false
+    @State private var numbersMessage : String = ""
     @State private var numberOfTimes = 0
+    @State private var unhiddenMessage : String = ""
+    @State private var computing = false
+    
     let clipboard = UIPasteboard.general
+    let decodeFuncs = Decode()
+    
+    private func compute() {
+        computing = true
+        unhiddenMessage = decodeFuncs.decodeInts(input: numbersMessage, numberOfIterations: numberOfTimes)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            computing = false
+        }
+    }
     
     var body: some View {
         Form(content: {
             SwiftUI.Section("Enter your message you want to show") {
                 TextField("Enter your message to decode", text: $numbersMessage)
+                    .disableAutocorrection(true)
+                    .onChange(of: numbersMessage) { _ in
+                        compute()
+                    }
                 Picker("How many times is message decoded", selection: $numberOfTimes) {
-                    ForEach(0...10, id: \.self){ number in
+                    ForEach(0...5, id: \.self){ number in
                         Text(String(number))
                     }
                 }.pickerStyle(.menu)
+                    .onChange(of: numberOfTimes) { _ in
+                        compute()
+                    }
+            }
+            if computing {
+                Text("Computing")
             }
 
             SwiftUI.Section("Your decoded message"){
-                Text("\"\(decodeFuncs.decodeInts(input: numbersMessage, numberOfIterations: numberOfTimes))\"")
+                Text(unhiddenMessage)
                 if copied {
                     Text("Copied!").foregroundColor(.red).bold().font(.title)
                 } else {
                     Button("Copy To clipboard") {
-                        clipboard.string = decodeFuncs.decodeInts(input: numbersMessage, numberOfIterations: numberOfTimes)
+                        clipboard.string = unhiddenMessage
                         copied = true
                         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                             copied = false
